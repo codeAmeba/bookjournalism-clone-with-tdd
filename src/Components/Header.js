9775fcdb2365736b0b806544545ae08e66f4ad97
jeongsooyoung <code.ameba@gmail.com>
2020-07-27 20:42:43 +0900

    Add: header component styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,13 @@ const HeaderContainer = styled.nav`
 	width: 100%;
 	height: 10%;
 	position: fixed;
+	top: 0;
 	background-color: blue;
+	transition: .5s ease-in-out;
+
+	@media only screen and (max-width: 990px) {
+		height: 55px;
+	}
 `;
 
 export default Header;
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from '../Components/Header';
 
 const Main = () => {
 	return (
-		<MainContainer data-testid='MainContainer'>
-			<Header />
-		</MainContainer>
+		<React.Fragment>
+			<GlobalStyle />
+			<MainContainer data-testid='MainContainer'>
+				<Header />
+			</MainContainer>
+		</React.Fragment>
 	);
 };
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #fff;
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+	}
+`;
 
 const MainContainer = styled.main`
 	width: 100%;
 	height: 100%;
+	position: relative;
 `;
 
 export default Main;
