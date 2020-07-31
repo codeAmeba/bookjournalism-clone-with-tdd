bf8d1f427465041368a6d9e544e9e5e8ee3b2bbc
jeongsooyoung <code.ameba@gmail.com>
2020-07-31 20:06:44 +0900

    MODIFY: modified layout

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import { AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineClose, AiOutlineGithub, AiOutlineInstagram, AiOutlineFacebook } from 'react-icons/ai';
+import { FaBlogger } from 'react-icons/fa';
 
 const Info = ({ infoOpenToggle }) => {
 	return (
 		<InfoContainer data-testid='Info'>
 			<AiOutlineClose className='closeIcon' data-testid='closeIcon' onClick={infoOpenToggle} />
+			<img
+				src='https://avatars1.githubusercontent.com/u/48628740?s=460&u=bc89f324605e79f99a8c048bf20fdc1b8c0da154&v=4'
+				alt='codeAmeba pofile'
+			/>
+			<div className='infoBox'>
+				<h3>안녕하세요 정수영입니다.</h3>
+				<p>이 사이트는 TDD를 연습하기 위해 만든 북저널리즘 클론 사이트입니다.</p>
+				<div className='iconBox'>
+					<a href='https://github.com/codeAmeba' target='_blank' rel='noopener noreferrer'>
+						<AiOutlineGithub className='githubIcon' />
+					</a>
+					<a href='https://codeameba.netlify.app/' target='_blank' rel='noopener noreferrer'>
+						<FaBlogger className='blogIcon' />
+					</a>
+					<a href='https://www.instagram.com/aprilgreenery/' target='_blank' rel='noopener noreferrer'>
+						<AiOutlineInstagram className='instaIcon' />
+					</a>
+					<a href='https://www.facebook.com/aprilgreenery' target='_blank' rel='noopener noreferrer'>
+						<AiOutlineFacebook className='faceIcon' />
+					</a>
+				</div>
+			</div>
 		</InfoContainer>
 	);
 };
 
 const InfoSlideAnimation = keyframes`
 	0% {
-		bottom: -300px;
+		bottom: -250px;
 		opacity: 0;
 	}
 	100% {
@@ -23,13 +46,16 @@ const InfoSlideAnimation = keyframes`
 
 const InfoContainer = styled.aside`
 	width: 100%;
-	height: 300px;
+	height: 250px;
 	background-color: #fff;
 	position: fixed;
 	bottom: 0;
 	z-index: 20;
 	animation: ${InfoSlideAnimation} .3s ease-in-out;
 	border-top: 1px solid rgba(0, 0, 0, 1);
+	display: flex;
+	justify-content: center;
+	align-items: flex-end;
 
 	.closeIcon {
 		font-size: 25px;
@@ -40,6 +66,44 @@ const InfoContainer = styled.aside`
 
 		&:hover {
 			color: #0166ff;
+		}
+	}
+
+	img {
+		width: 250px;
+		height: auto;
+	}
+
+	.infoBox {
+		display: flex;
+		justify-content: center;
+		align-items: flex-end;
+		flex-direction: column;
+		margin-bottom: 45px;
+		padding: 0 20px;
+
+		h3 {
+			font-size: 26px;
+			border-bottom: 3px solid rgba(0, 0, 0, 1);
+			margin: 0;
+		}
+
+		.iconBox {
+			font-size: 30px;
+			height: 30px;
+			width: 150px;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			cursor: pointer;
+
+			a {
+				color: rgba(0, 0, 0, 1);
+			}
+
+			& > *:hover {
+				color: #0166ff;
+			}
 		}
 	}
 `;
