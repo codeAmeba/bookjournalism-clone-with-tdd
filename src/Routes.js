0f50113d19228a7908c4625737487cde24da23a2
jeongsooyoung <code.ameba@gmail.com>
2020-08-04 01:21:30 +0900

    remove router

--- src/Routes.js
+++ src/Routes.js
@@ -8,7 +8,7 @@ const Routes = () => {
 	return (
 		<Router>
 			<Switch>
-				<Route exact path='/bookjournalism-clone-with-tdd' component={Main} />
+				<Route exact path='/' component={Main} />
 				<Route exact path='/login' component={Login} />
 				<Route exact path='/detail' component={Detail} />
 				<Route exact path='/detail/:id' component={Detail} />
